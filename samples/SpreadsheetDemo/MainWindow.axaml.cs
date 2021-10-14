1325bbadcb161e82a0c2d8a1dc2033b634dec207
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-10-14 11:12:59 +0200

    Add OpenXml sample

--- samples/SpreadsheetDemo/MainWindow.axaml.cs
+++ samples/SpreadsheetDemo/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -19,6 +21,62 @@ namespace SpreadsheetDemo
             DemoSpreadsheet();
         }
 
+        public async Task OpenSpreadsheet()
+        {
+            var dlg = new OpenFileDialog() { Title = "Open" };
+            dlg.Filters.Add(new FileDialogFilter() { Name = "XLSX Files", Extensions = { "xlsx" } });
+            dlg.Filters.Add(new FileDialogFilter() { Name = "All", Extensions = { "*" } });
+            var result = await dlg.ShowAsync(this);
+            var path = result?.FirstOrDefault();
+            if (path is { })
+            {
+                var items = OpenXmlReader.Read(path);
+                if (items is { })
+                {
+                    var spreadsheet = this.FindControl<SpreadsheetControl>("Spreadsheet");
+                    var columnWidth = 130;
+                    var rowHeight = 28;
+                    var columns = new List<Column>();
+                    var rows = new List<Row>();
+
+                    var columnsCount = items.First().Count;
+                    var rowsCount = items.Count();
+        
+                    for (var r = 0; r < rowsCount; r++)
+                    {
+                        var row = new Row
+                        {
+                            Header = $"{r}",
+                            Height = rowHeight,
+                            Index = r
+                        };
+                        rows.Add(row);
+                    }
+        
+                    for (var c = 0; c < columnsCount; c++)
+                    {
+                        var column = new Column
+                        {
+                            Header = $"{c}",
+                            Width = columnWidth,
+                            Index = c
+                        };
+                        columns.Add(column);
+                    }
+
+                    spreadsheet.RowHeadersWidth = 130;
+                    spreadsheet.ColumnHeadersHeight = 28;
+        
+                    spreadsheet.Columns.Clear();
+                    spreadsheet.Rows.Clear();
+                    
+                    spreadsheet.Columns.AddRange(columns);
+                    spreadsheet.Rows.AddRange(rows);
+                    spreadsheet.Items = items;
+                }
+            }
+        }
+        
         private void DemoSpreadsheet()
         {
             var spreadsheet = this.FindControl<SpreadsheetControl>("Spreadsheet");
---
+++ samples/SpreadsheetDemo/OpenXmlReader.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace SpreadsheetDemo
+{
+    public class OpenXmlReader
+    {
+        private static string? ToString(Cell c, SharedStringTablePart? stringTable)
+        {
+            if (c.DataType is null)
+            {
+                return c.CellValue?.Text;
+            }
+
+            switch (c.DataType.Value)
+            {
+                case CellValues.SharedString:
+                {
+                    if (stringTable is { })
+                    {
+                        int index = int.Parse(c.InnerText);
+                        var value = stringTable.SharedStringTable.ElementAt(index).InnerText;
+                        return value;
+                    }
+                }
+                    break;
+                case CellValues.Boolean:
+                {
+                    return c.InnerText switch
+                    {
+                        "0" => "FALSE",
+                        _ => "TRUE",
+                    };
+                }
+                case CellValues.Number:
+                    return c.InnerText;
+                case CellValues.Error:
+                    return c.InnerText;
+                case CellValues.String:
+                    return c.InnerText;
+                case CellValues.InlineString:
+                    return c.InnerText;
+                case CellValues.Date:
+                    return c.InnerText;
+            }
+
+            return null;
+        }
+
+        public static List<List<object?>>? Read(Stream stream)
+        {
+            var spreadsheetDocument = SpreadsheetDocument.Open(stream, false);
+
+            var workbookPart = spreadsheetDocument.WorkbookPart;
+            if (workbookPart is null)
+            {
+                return null;
+            }
+
+            var items = new List<List<object?>>();
+
+            var worksheetPart = workbookPart.WorksheetParts.First();
+
+            var sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+
+            var stringTable = workbookPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+
+            foreach (var row in sheetData.Elements<Row>())
+            {
+                var fields = row.Elements<Cell>().Select(c => (object?)ToString(c, stringTable)).ToList();
+                items.Add(fields);
+            }
+
+            spreadsheetDocument.Close();
+
+            return items;
+        }
+
+        public static List<List<object?>>? Read(string path)
+        {
+            using var stream = File.OpenRead(path);
+            var items = Read(stream)?.ToList();
+            return items;
+        }
+    }
+}
